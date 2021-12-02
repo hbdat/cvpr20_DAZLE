196372ddb1890246cbf27475f3ac373b8fb6ac46
dathuynh <60049111+hbdat@users.noreply.github.com>
2021-12-01 21:34:56 -0500

    Update CUBDataLoader.py

--- core/CUBDataLoader.py
+++ core/CUBDataLoader.py
@@ -26,7 +26,7 @@ mat_path = os.path.join(NFS_path,'data/xlsa17/data/CUB/res101.mat')
 attr_path = './attribute/CUB/new_des.csv'
 
 class CUBDataLoader():
-    def __init__(self, data_path, device, is_scale = False,is_unsupervised_attr = False,is_balance=True):
+    def __init__(self, data_path, device, is_scale = False, is_balance=True):
 
         print(data_path)
         sys.path.append(data_path)
@@ -44,7 +44,6 @@ class CUBDataLoader():
         self.is_balance = is_balance
         if self.is_balance:
             print('Balance dataloader')
-        self.is_unsupervised_attr = is_unsupervised_attr
         self.read_matdataset()
         self.get_idx_classes()
     
